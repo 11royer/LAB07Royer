f1ec37176f76dc8e0dcf68ae516bc3b8d0469990
royer <royermartinezperez@gmail.com>
2024-04-13 17:43:31 -0300

    laboratorio7

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css'
+import { List } from './componentes/List'
+import { WapperList } from './componentes/WrapperList'
+
+function App(){
+  return(
+  <>
+    <WapperList>
+      <List/>
+    </WapperList>
+  </>
+  )
+}
+/*import { Button } from './componentes/Button'
+function App() {
+
+  const buttonText = "Count : "
+  return (
+    <>
+      <h1>probando  en vite</h1>
+      <Button text={buttonText}/>
+    </>
+  )
+}*/
+
+export default App
